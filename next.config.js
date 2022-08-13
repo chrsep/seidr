e96e22ed72374999811b4140a9582cfeaca6d4c3
Chrisando E. Pramudhita <chrsep@protonmail.com>
2022-08-13 09:38:53 +0700

    try modern js output.

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const withPreact = require("next-plugin-preact")
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  experimental: {
+    browsersListForSwc: true,
+    legacyBrowsers: false
+  }
 }
 
 module.exports = withPreact(nextConfig)
